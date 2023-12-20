054f3a11a2ffce91ca3595a0956977cc81c174e4
fpadula2 <fmello.85@gmail.com>
2023-12-20 03:13:41 -0300

    mobile finalizado

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const menuBtn = document.querySelector("header button");
+const burgerBars = document.querySelector(".fa-bars");
+const closeBtn = ["X", burgerBars];
+const burgerBtn = [menuBtn];
+const teste = document.querySelector("header i")
+console.log(teste);
+
 
 menuBtn.addEventListener("click", () => {
-    document.querySelector("#mobile").classList.toggle("show-menu__mobile")
-
+    let toggleMenu = document.querySelector("#mobile").classList.toggle("show-menu__mobile")
 })
+
+function animar() {
+    const menuBtn = document.querySelector("header button");
+    menuBtn.classList.toggle("ativar")
+}
+
+
+// menuBtn.addEventListener("click", () => {
+//     let toggleMenu = document.querySelector("#mobile").classList.toggle("show-menu__mobile")
+//     //     if (toggleMenu) {
+//     //         menuBtn.innerText = closeBtn[0];
+//     //     } else {
+//     //         teste;
+//     //     }
+// })
