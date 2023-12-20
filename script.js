0cc02741bc6098b277a4efe5bb6d7aff146bf338
fpadula2 <fmello.85@gmail.com>
2023-12-19 23:17:01 -0300

    adicionado js

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector("header button");
+
+menuBtn.addEventListener("click", () => {
+    document.querySelector("#mobile").classList.toggle("show-menu__mobile")
+
+})
